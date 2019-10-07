e03efb409fa4d202ea10577b2f542e99f52dfd94
Saulo de Carvalho Neto <sacarvalho@uolinc.com>
2019-10-07 20:28:41 -0300

    Slices 2

--- cursodego/intermediario/slices/main.go
+++ cursodego/intermediario/slices/main.go
@@ -13,12 +13,13 @@ func main() {
 	capitais = append(capitais, "Brasília")
 	fmt.Println(capitais, len(capitais), cap(capitais))
 
-	cidades := make([]string, 4)
+	cidades := make([]string, 5)
 
 	cidades[0] = "Nova Iorque"
 	cidades[1] = "Londres"
 	cidades[2] = "Tókio"
-	cidades[3] = "Taubaté"
+	cidades[3] = "Ubatuba"
+	cidades[4] = "Taubaté"
 	fmt.Println(cidades, len(cidades), cap(cidades))
 
 	capitais[1] = "Brasilia"
@@ -27,4 +28,20 @@ func main() {
 	for indice, cidade := range cidades {
 		fmt.Printf("Cidade[%d] = %s\r\n", indice, cidade)
 	}
+
+	cidadesBrasil := cidades[3:5]
+	fmt.Println(cidadesBrasil, len(cidadesBrasil), cap(cidadesBrasil))
+
+	temp1 := cidades[:2]
+	fmt.Println(temp1, len(temp1), cap(temp1))
+
+	temp2 := cidades[len(cidades)-2:]
+	fmt.Println(temp2, len(temp2), cap(temp2))
+
+	indiceDoItemARetirar := 2
+	temp := cidades[:indiceDoItemARetirar]
+	temp = append(temp, cidades[indiceDoItemARetirar+1:]...)
+	copy(cidades, temp)
+	fmt.Println(temp, len(temp), cap(temp))
+
 }
